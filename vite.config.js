456e0db1aa2d17ff68c4e78e01f2db39c6487fcd
ti4g <tiagosousaquixabaaa@gmail.com>
2026-04-05 17:22:06 -0300

    att

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/newportifolio/",  // ← tem que bater com o nome exato do repo
+  base: "/myportifolio/",  // ← tem que bater com o nome exato do repo
 });
